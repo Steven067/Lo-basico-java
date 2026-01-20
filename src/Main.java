f22f08b2885c4fb5776c17d9b486a3d4dbf9582b
Alejandro <stevenarenas74@gmail.com>
2026-01-20 10:57:44 -0500

    Variables en Java

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args){
+
+//VARIABLES EN JAVA
+        // Para declarar una variable primero se coloca el tipo de variable, luego en nombre de la varibale y por ultimo le damos valor a esa variable
+
+ //TIPOS DE VARIABLES
+
+        //NUMEROS ENTEROS
+        byte variableByte = 127;
+        short variableShort = 31000;
+        int variableInt = 1000000000;
+        long variableLong= 1000000000000000000l;
+
+
+        //NUMEROS REALES
+        float variableFloat = 1.555f;
+        double variableDouble = 1.55555555555;
+
+        //BOOLEANOS
+        boolean variableBoolean = true;
+
+        //CADENA Y CARACTERES
+        String variableString = "Hola mundo";
+        char variableChar = 'a';
+
+        //MOSTRAR VALORES
+        System.out.println ("Variable byte:" + variableByte);
+        System.out.println ("Variable short:" + variableShort);
+    }
+}
